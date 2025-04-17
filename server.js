6227e51b82297ba5372d914c8f6ef45d6c5ddd62
slim msehli <dmsehlislim@gmail.com>
2025-04-18 01:01:48 +0200

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const http = require('http');
 const { Server } = require('socket.io');
 const { v4: uuidv4 } = require('uuid');
-
+const path = require('path');
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
